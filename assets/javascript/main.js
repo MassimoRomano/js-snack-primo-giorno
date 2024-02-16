2cf8c0b900ed5607a4d7e4da96b00109817162ff
MassimoRomano <romano93m@gmail.com>
2024-02-16 16:25:36 +0100

    add second var

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,5 +2,15 @@
 Esercizio 1
 Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
 */
-let number = 100
-console.log(number) 
+let number = 100 ;
+console.log(number) ; 
+
+/*
+Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+*/
+
+let PI ;
+PI = 3.14 ;
+console.log(PI)
+
