52dea40e0ed0774500cf246ddd432ef3e207793d
MassimoRomano <romano93m@gmail.com>
2024-02-16 17:09:05 +0100

    add tirth var

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@
 Esercizio 1
 Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
 */
-let number = 100 ;
+let number = 100 ; //number
 console.log(number) ; 
 
 /*
@@ -10,7 +10,7 @@ Esercizio 2
 Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
 */
 
-let PI ;
+let PI ; //string
 PI = 3.14 ;
 console.log(PI)
 
@@ -20,13 +20,39 @@ Esercizio 2B
 Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
 */
 
-/*const PI ;
-PI = 3.1416 ;
+/*const PI ; //string
+PI = 3.1416 ; //number
 console.log(PI) ;
 */
 
 
 /*
+La console da errore poiche' una variabile definita con const non puo' essere riassegnata o dichiarata nuovamente
 Uncaught SyntaxError: Identifier 'PI' has already been declared (at main.js:23:7)
 */
 
+
+/*
+Esercizio 2/C
+-Dichiara una variabile chiamata radius e assegnale il valore 8.
+-Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+-Stampa il valore della variabile circle in console.
+*/
+
+let radius = 8 ; //number
+circle = radius * 2 * PI // string * numer * string
+console.log(circle)
+
+/*
+Esercizio 3
+-Dichiara una variabile chiamata name e
+-assegnale il valore "Carlo".
+-Successivamente, modifica il valore della variabile in "Marco".
+-Stampa il nuovo valore in console.
+*/
+
+
+let name = "Carlo" ; //string
+name = "Marco" //string
+console.log(name)
+ 
